889f687a68d53e989b8004db8cb4f615a99971a9
lorenzhohermuth <lofloho@gmail.com>
2024-04-29 20:11:36 +0200

    changes idk

--- lua/lorenz/golsp-config.lua
+++ lua/lorenz/golsp-config.lua
@@ -1,17 +1,47 @@
-local nvim_lsp = require('lspconfig')
+local lspconfig = require('lspconfig')
 local lastRootPath = nil
 local gomodpath = vim.trim(vim.fn.system("go env GOPATH")) .. "/pkg/mod"
 
-nvim_lsp.gopls.setup({
+lspconfig.gopls.setup({
   root_dir = function(fname)
     local fullpath = vim.fn.expand(fname, ":p")
     if string.find(fullpath, gomodpath) and lastRootPath ~= nil then
         return lastRootPath
     end
-    local root = nvim_lsp.util.root_pattern("go.mod", ".git")(fname)
+    local root = lspconfig.util.root_pattern("go.mod", ".git")(fname)
     if root ~= nil then
         lastRootPath = root
     end
     return root
   end,
 })
+
+lspconfig.templ.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.tailwindcss.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+  init_options = { userLanguages = { templ = "html" } },
+})
+
+lspconfig.html.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "templ" },
+})
+
+lspconfig.htmx.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "templ" },
+})
+
+lspconfig.unocss.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "javascriptreact", "rescript", "typescriptreact", "vue", "svelte", "templ"}
+}
